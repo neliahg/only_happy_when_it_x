e92398be25af4f06a3ea66d21ce88ab243e1fbbc
neliahg <neliah.goldstein@gmail.com>
2025-07-26 12:30:07 +0300

    trial run

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("Life is Amazing")
-data = pd.DataFrame({"animal": ["cat", "dog"], "cute": [10, 8]})
+data = pd.DataFrame({"animal": ["cat", "dog","mouse"], "cute": [10, 8]})
 st.dataframe(data)
 
 secret = st.text_input("Type here:")
